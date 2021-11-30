10390d2685b992feafd1a64d914c48494e5ab7a3
Francesco <91691097+5291353@users.noreply.github.com>
2021-11-30 12:23:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
 
-int main(){
-  return 0;
+
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+    int numero=0, conta=0;
+    cin>>numero;
+    numprimo(numero,1,conta);
+    if(conta==0){
+        cout << "il numero e\' primo"<<endl;
+    }else{
+        cout<< "il numero non e\' primo"<<endl;
+    }
+
+    return 0;
 }
