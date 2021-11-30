7f0955b5447fff0a3a3e6745442009523c537525
Francesco <91691097+5291353@users.noreply.github.com>
2021-11-30 12:25:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
 
+#include "lib.h"
+#include <iostream>
+void numprimo(int a,int i, int &conta){
+    int div=0;
+    i++;
+    if(a>1 and a>i){
+        div= a%i;
+        if(div==0){
+            conta++;
+        }else{
+           numprimo(a,i,conta);
+        }
+    }
+
+}
